50885aa4a780fc2a561586f9f44c56da97bc2f66
BloodPS <153712377+BloodPS@users.noreply.github.com>
2025-02-21 21:24:59 +0700

    Update model.py

--- model.py
+++ model.py
@@ -84,7 +84,7 @@ def predict():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # ใช้ PORT จาก Railway
+    port = int(os.environ.get("PORT", 10000))  # ใช้ PORT จาก Railway
     app.run(debug=True,host="0.0.0.0", port=port)
 
 
