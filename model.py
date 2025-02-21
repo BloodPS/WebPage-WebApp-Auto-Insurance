1a80f97b6eb40fc78a9908575ef0f68b6ff89d92
BloodPS <153712377+BloodPS@users.noreply.github.com>
2025-02-21 16:44:10 +0700

    Update model.py

--- model.py
+++ model.py
@@ -82,8 +82,10 @@ def predict():
         'claims_severity': claims_severity
     })
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # ใช้ PORT จาก Railway
+    app.run(debug=True,host="0.0.0.0", port=port)
 
 
 
